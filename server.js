781c2b5961c73743b3411692bff6b426fad6ecd9
MARI0-ip <rubenmarioicazaperedo@gmail.com>
2026-05-29 13:19:05 -0400

    fix: add X-Accel-Buffering header to SSE endpoint for nginx/EasyPanel compatibility

--- server.js
+++ server.js
@@ -331,6 +331,7 @@ app.get('/api/eventos', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
+  res.setHeader('X-Accel-Buffering', 'no');
   res.flushHeaders();
 
   res.write(`data: ${JSON.stringify({
